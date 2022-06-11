143e7c6a26752e66501a89cc4c4a62fc193e542e
mooziii <63669478+mooziii@users.noreply.github.com>
2022-06-11 18:44:07 +0200

    Update yarn

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     minecraft("com.mojang:minecraft:1.19")
-    mappings("net.fabricmc:yarn:1.19+build.1")
+    mappings("net.fabricmc:yarn:1.19+build.2")
     modImplementation("net.fabricmc:fabric-loader:0.14.6")
     modImplementation("net.fabricmc:fabric-language-kotlin:1.7.4+kotlin.1.6.21")
 }
